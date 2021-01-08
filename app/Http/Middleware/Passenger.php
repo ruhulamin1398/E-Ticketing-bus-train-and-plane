4785b61915a8910f9e8d2148e4b2342d14cc5f48
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-08 12:10:45 +0600

    added auth chcek in model

--- app/Http/Middleware/Passenger.php
+++ app/Http/Middleware/Passenger.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Passenger
 {
@@ -15,6 +16,11 @@ class Passenger
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::User()->isPassenger()){
+            return $next($request);
+        }else{
+
+            return "You Are not Allowed";
+        }
     }
 }
