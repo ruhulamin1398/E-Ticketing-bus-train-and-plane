eb81158f7b1cc5d69be487f7da84f96b424bab42
masumbillah3416 <masumbillahsujon1998@gmail.com>
2021-01-08 18:40:14 +0600

    status and seat

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\status;
+use Faker\Generator as Faker;
+
+$factory->define(status::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
