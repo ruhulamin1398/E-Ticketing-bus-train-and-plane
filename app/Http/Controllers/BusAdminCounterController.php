90795a6d650e4207e59e2bc4a7597c0cf9ab67c7
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-10 00:51:04 +0600

    where querry

--- app/Http/Controllers/BusAdminCounterController.php
+++ app/Http/Controllers/BusAdminCounterController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\busCounter;
 use App\busSchedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BusAdminCounterController extends Controller
 {
     public function index()
     {
         
-        $schedules= busSchedule::all();
+        $counter_id = Auth::user()->counter_id;
+        $counter = busCounter::find($counter_id);
+        $destination_id = $counter->destination_id;
+        
+        $schedules= busSchedule::where('from_destination_id',$destination_id)->get();
         return view('bus.counter.index',compact('schedules'));
     }
 }
