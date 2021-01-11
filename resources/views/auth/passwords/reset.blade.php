a1931acf3140decd92dfe2c9bba319c36029e5cb
masumbillah3416 <masumbillahsujon1998@gmail.com>
2021-01-11 18:18:18 +0600

    login

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,69 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Ticket</title>
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('file/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="{{asset('file/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template-->
+    <!-- Custom style-->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <style type="text/css">
+        .bg-dark-color {
+            background-color: #2a3f5c;
+            color: #fff;
+
+
+        }
+
+    </style>
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('file/jquery/jquery.min.js')}}"></script>
+
+
+    <script src="{{asset('file/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('file/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('file/chart.js/Chart.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('js/demo/chart-pie-demo.js')}}"></script>
+
+
+
+    <!-- Page level plugins -->
+    <script src="{{asset('file/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('file/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
+
+</head>
+
+<body>
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -62,4 +125,5 @@
         </div>
     </div>
 </div>
-@endsection
+</body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,16 +8,58 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+ <!-- Custom fonts for this template-->
+ <link href="{{asset('file/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+ <!-- Custom styles for this template -->
+ <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+ <!-- Custom styles for this page -->
+ <link href="{{asset('file/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+
+ <!-- Custom styles for this template-->
+ <!-- Custom style-->
+ <link href="{{asset('css/style.css')}}" rel="stylesheet">
+ <style type="text/css">
+     .bg-dark-color {
+         background-color: #2a3f5c;
+         color: #fff;
+
+
+     }
+
+ </style>
+
+
+ <!-- Bootstrap core JavaScript-->
+ <script src="{{asset('file/jquery/jquery.min.js')}}"></script>
+
+
+ <script src="{{asset('file/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+ <!-- Core plugin JavaScript-->
+ <script src="{{asset('file/jquery-easing/jquery.easing.min.js')}}"></script>
+
+ <!-- Custom scripts for all pages-->
+ <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+
+ <!-- Page level plugins -->
+ <script src="{{asset('file/chart.js/Chart.min.js')}}"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
+ <script src="{{asset('js/demo/chart-pie-demo.js')}}"></script>
+
+
+
+ <!-- Page level plugins -->
+ <script src="{{asset('file/datatables/jquery.dataTables.min.js')}}"></script>
+ <script src="{{asset('file/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
 </head>
 <body>
     <div id="app">
